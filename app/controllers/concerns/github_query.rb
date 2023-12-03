bb6e64ff4f04a49c5976b71890c1d211e0907635
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-12-03 17:54:14 +0200

    upd graph request

--- app/controllers/concerns/github_query.rb
+++ app/controllers/concerns/github_query.rb
@@ -4,12 +4,20 @@ module GithubQuery
   extend ActiveSupport::Concern
 
   QueryString = Github::Client.parse <<~GRAPHQL
-    query($login: String!) {
-      user(login: $login) {
-        name,
-        repositories(first: 10) {
-          nodes {
-            name
+        query($login: String!, $pageSize: Int!, $cursor: String) {
+          user(login: $login) {
+            name,
+        repositories(first: $pageSize, after: $cursor) {
+          edges {
+            node {
+              name
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+            startCursor
           }
         }
       }
