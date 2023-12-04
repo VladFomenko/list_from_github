775b6966ba977cee1da60b4416460173a1d41d60
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-12-04 12:11:33 +0200

    xRevert "fix tests"
    This reverts commit d88446f3fe3bf1fb5b393c98e5e30481826ac874.

--- spec/requests/git_hub_spec.rb
+++ spec/requests/git_hub_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe GitHubController, type: :controller do
   describe 'GET #show' do
     it 'renders the show template' do
       VCR.use_cassette('github_user_data') do
-        get :show, params: { login: 'Vladyslav Fomenko' }
+        get :show, params: { login: 'test_user' }
         expect(response).to render_template(:show)
       end
     end
 
     it 'fetches user data from GitHub API' do
       VCR.use_cassette('github_user_data') do
-        get :show, params: { login: 'Vladyslav Fomenko' }
+        get :show, params: { login: 'test_user' }
         expect(assigns(:user_data)).to eq({ name: 'Vladyslav Fomenko',
                                             repo: %w[RubyHW ror_geekhub_jira ror_geekhub_jira hospital_control api
                                                      test_deploy_netlify] })
@@ -23,6 +23,15 @@ RSpec.describe GitHubController, type: :controller do
     it 'renders the new template' do
       get :new
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe '#show' do
+    it 'gets user data from GitHub' do
+      VCR.use_cassette('github_user_data') do
+        get :show, params: { login: 'test_user' }
+        expect(response).to be_successful
+      end
     end
   end
 end
