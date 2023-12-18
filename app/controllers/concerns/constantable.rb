e5b984e49117b7ec05985bc309b8cbf42a99156f
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-12-18 12:06:50 +0200

    delete comments

--- app/controllers/concerns/constantable.rb
+++ app/controllers/concerns/constantable.rb
@@ -3,32 +3,9 @@
 module Constantable
   extend ActiveSupport::Concern
 
-  PAGE_SIZE = 16
   QUERY = <<~GRAPHQL
     query($login: String!) {
       gitDataUser(login: $login)
     }
   GRAPHQL
-
-  # TODO: upd query
-  # QueryString = <<~GRAPHQL
-  #       query($login: String!, $pageSize: Int!, $cursor: String) {
-  #         user(login: $login) {
-  #           name,
-  #       repositories(first: $pageSize, after: $cursor) {
-  #         edges {
-  #           node {
-  #             name
-  #           }
-  #           cursor
-  #         }
-  #         pageInfo {
-  #           hasNextPage
-  #           endCursor
-  #           startCursor
-  #         }
-  #       }
-  #     }
-  #   }
-  # GRAPHQL
 end
